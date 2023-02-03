66b12b9182324415b07a600b13c3b0bd5065f681
luc torres <luctiretdu6torres@gmail.com>
2023-02-03 11:19:46 +0100

    save - luc

---
+++ script.js
@@ -0,0 +1,15 @@
+document.querySelector(".app-bar").innerHTML = `
+<div>
+	<div>
+		<img src="assets/logoMondeDeDemain.png" alt="logo">
+	</div>
+</div>
+<div>
+	<a href="">Accueil</a>
+	<a href="">En bref</a>
+	<a href="">Actualités</a>
+	<a href="">Dimensions</a>
+	<a href="">Medias</a>
+	<a href="">FAQ</a>
+</div>
+`
