a98dbd8b4c89078044adbdafc9456569568b8e36
luc torres <luctiretdu6torres@gmail.com>
2023-02-03 11:52:51 +0100

    splash screen - luc

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ fetch("components/app_bar.htm").then(function (response) {
 	return response.text()
 }).then((html) => {
 	document.querySelector(".app-bar").innerHTML = html;
+	setTimeout(() => {
+		document.querySelector(".splash-screen").style.display = "none";
+	}, 300);
 }).catch(function (err) {
 	console.log('Failed to fetch page: ', err);
 });
