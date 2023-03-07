77c6c8c09194f2d5a458f2af1bbc9b92931fc8b9
unknown <suarezmathieu.icn@gmail.com>
2023-03-08 00:20:33 +0100

    carousel page accueil responsif

--- accueil/script.js
+++ accueil/script.js
@@ -20,13 +20,25 @@ setInterval(() => {
 // Caroussel Rond
 $(document).ready(function() {
   $("#news-slider").owlCarousel({
-      items : 5,
-      itemsDesktop:[1199,3],
-      itemsDesktopSmall:[980,2],
-      itemsMobile : [600,1],
       navigation:true,
       navigationText:["",""],
       pagination:true,
-      autoPlay:true
+      autoPlay:true,
+      responsiveClass:true,
+        responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:5,
+            nav:true,
+            loop:false
+        }
+    }
   });
 });
